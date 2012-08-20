d20c09e75961468b21a154e789b1d5d52dbacd95
mr.stobbe <mr.stobbe@gmail.com>
2012-08-20 18:41:11 +0000

    [unstable] Started work on Poller

--- src/bin-tests/foo.cpp
+++ src/bin-tests/foo.cpp
@@ -1,3 +1,10 @@
 
+#include <Renode.h>
+
+#include <iostream>
+
+using namespace std;
+
 int main(int argc, char** argv) {
+	RN::Poller poller;
 };
--- src/include/Renode.h
+++ src/include/Renode.h
@@ -1,9 +1,41 @@
 #ifndef Renode_H_
 #define Renode_H_
 
-namespace rn {
-};
+#include <Renode/Definitions.h>
+
+#if __cplusplus
+namespace RN {
+
+	namespace Net {
+		template<class T> class Addr;
+
+		namespace Stream {
+			class Addr;
+			class Client;
+			class Server;
+			class Service;
+		}; //ns Stream
+
+		namespace Packet {
+			class Addr;
+			class Client;
+			class Server;
+			class Service;
+		}; //ns Packet
+	}; //ns Net
+
+	class Poller;
+}; //ns RN
+
+extern "C" {
+#endif /* __cplusplus */
 
 
+
+#if __cplusplus
+} //extern "C"
+#endif /* __cplusplus */
+
+#include <Renode/Poller.h>
 
 #endif /* Renode_H_ */
